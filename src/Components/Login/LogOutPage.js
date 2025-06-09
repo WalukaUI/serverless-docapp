6cc24569b3d3718c3dcc359dce18b19b3823fb4c
walukaui <cwaluka@yahoo.com>
2025-06-09 17:03:54 -0500

    added styling to logout landing page

--- src/Components/Login/LogOutPage.js
+++ src/Components/Login/LogOutPage.js
@@ -3,7 +3,7 @@ function LogoutLandingPage() {
 
     const signOutRedirect = () => {
     const clientId = "2kih0h4ra0c1ognli9tomv1hs7";
-    const logoutUri = "https://main.d11x0rh1r8wz2j.amplifyapp.com";
+    const logoutUri = "http://localhost:3000";
     const cognitoDomain = "https://us-east-2smuhwfuds.auth.us-east-2.amazoncognito.com";
     window.location.href = `${cognitoDomain}/logout?client_id=${clientId}&logout_uri=${encodeURIComponent(logoutUri)}`;
   };
@@ -12,13 +12,14 @@ function LogoutLandingPage() {
     return<>
     <div className="logoutLandingPageDiv">
       <h2>"You have Successfully logged Out" </h2>
-      <button onClick={() => signOutRedirect()}>Sign out</button>
       <h3>"I'm sending you a virtual hug and wishes of good health!"</h3>
       <img
               src="../hospital logo.png"
               alt="Logo"
               style={{ width: "90px", marginLeft: "10px" }}
-            />
+            /><br/>
+      <p>Click below button, If you wish to clear browser data related to this website</p>
+      <button className="btn btn-warning styled-button" onClick={() => signOutRedirect()}>Clear Browser Data</button>
     </div>
     </>
 }
