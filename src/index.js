1749c6471f44c412687e7b2d419e90e9436e936e
walukaui <cwaluka@yahoo.com>
2025-06-30 15:22:39 -0500

    removed restrict mode

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import reportWebVitals from './reportWebVitals';
 const cognitoAuthConfig = {
   authority: "https://cognito-idp.us-east-2.amazonaws.com/us-east-2_SmuHWFUDs",
   client_id: "2kih0h4ra0c1ognli9tomv1hs7",
-  redirect_uri: "http://localhost:3000",
+  redirect_uri: "https://main.d11x0rh1r8wz2j.amplifyapp.com",
   response_type: "code",
   scope: "email openid phone",
 };
@@ -18,12 +18,12 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 // http://localhost:3000/
 // wrap the application with AuthProvider
 root.render(
-  <React.StrictMode>
+
     <AuthProvider {...cognitoAuthConfig}>
       <BrowserRouter>
       <App />
       </BrowserRouter>
     </AuthProvider>
-  </React.StrictMode>
 );
 reportWebVitals();
+//  <React.StrictMode>
